2612716df7c9873a21ed6f6d8a201737057e0257
Suparna Ghanvatkar <suparna-ghanvatkar@users.noreply.github.com>
2017-03-17 23:51:11 +0530

    option of not listing to do

--- reminder.sh
+++ reminder.sh
@@ -1,10 +1,13 @@
-grep $(date +%d-%m) todo.dat
-grep -n 'T' todo.dat 
-echo "Enter the tasks to be marked as done"
-read -a arr
-echo ${#arr[@]}
-for i in ${arr[@]}
-do
-    echo "${i}s/T/D"
-    sed -i "${i}s/T/D/" todo.dat
-done
+echo "Type L to list the todo"
+read -sn1 c
+if [ $c = "L" ]; then
+    grep $(date +%d-%m) todo.dat
+    grep -n 'T' todo.dat 
+    echo "Enter the tasks to be marked as done"
+    read -a arr
+    echo ${#arr[@]}
+    for i in ${arr[@]}
+    do
+        sed -i "${i}s/T/D/" todo.dat
+    done
+fi
